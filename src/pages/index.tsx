77621ea6d3ebc5978b530d2c3f62ceb78bd44d32
ingingcat <onukim0129@gmail.com>
2026-03-28 20:35:38 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,11 @@ export default function Index() {
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
         {/* 제목만 남기고 다 지웠습니다 */}
-        <h1>My Notion Blog</h1>
-        <h2>Welcome to my blog!</h2> 
+        <h1>Onyu Kim portfolio</h1>
+        <h2>Welcome to my portfolio!</h2> 
         
-        {/*김온유 학생의 포트폴리오입니다*/ :)}
+        // 기존 13번 줄을 지우고 아래 내용으로 바꾸세요
+          <p>김온유 학생의 포트폴리오입니다 :)</p>
         <div className="explanation">
           <p>노션으로 만드는 나만의 블로그입니다.</p>
         </div>
