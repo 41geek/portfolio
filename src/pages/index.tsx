e02d7949ba0c2d1d266e9d9ccf819ceca69c5b82
ingingcat <onukim0129@gmail.com>
2026-03-28 20:37:10 +0900

    Revise portfolio introduction in index.tsx
    Updated portfolio introduction text.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ export default function Index() {
         <h1>Onyu Kim portfolio</h1>
         <h2>Welcome to my portfolio!</h2> 
         
-        // 기존 13번 줄을 지우고 아래 내용으로 바꾸세요
           <p>김온유 학생의 포트폴리오입니다 :)</p>
         <div className="explanation">
           <p>노션으로 만드는 나만의 블로그입니다.</p>
